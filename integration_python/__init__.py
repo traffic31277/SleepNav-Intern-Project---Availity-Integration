fb3c6a9e35d70b73bef3116b8dfa605ecf4b701e
lily williams <dangerduck.jpeg@gmail.com>
2026-06-24 08:03:24 -0500

    made python module

---
+++ integration_python/__init__.py
@@ -0,0 +1,13 @@
+import AvailityAbstract
+import AvailityCoverages
+import AvailityServiceReviews
+import AvailityFunctions
+import AvailityPayerList
+
+def __init__():
+    """
+    This is the middleware for Availity's healthcare transactions API
+    """
+
+    pass
+
